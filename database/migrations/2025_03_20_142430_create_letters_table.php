4c7284b7ff29cda5e039d602df7972d8485e3424
Gilang <19211020@bsi.ac.id>
2025-03-23 20:25:07 +0700

    feat(database/migrations): add letter upload & directs table

--- database/migrations/2025_03_20_142430_create_letters_table.php
+++ database/migrations/2025_03_20_142430_create_letters_table.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('letters', function (Blueprint $table) {
             $table->id();
+            $table->varchar(255, 'category_type');
+            $table->unsignedBigInteger('category_id');
+            $table->varchar(255, 'responsible_person');
+            $table->varchar(255,'reference_number');
+            $table->enum('type', ['upload', 'direct']);
             $table->timestamps();
+        });
+
+        Schema::create('letter_uploads', function (Blueprint $table) {
+            $table->id();
+            $table->varchar(255, 'file_name');
+            $table->varchar(255, 'file_path');
+        });
+
+        Schema::create('letter_directs', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
         });
     }
 
@@ -23,5 +39,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('letters');
+        Schema::dropIfExists('letter_upload');
+        Schema::dropIfExists('letter_direct');
     }
 };
