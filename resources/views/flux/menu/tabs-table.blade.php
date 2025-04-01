cbfa681ab84556e47cea1c844d7128813089ca5e
Gilang <19211020@bsi.ac.id>
2025-04-01 20:32:22 +0700

    feat(views/flux): add tabs table component for status filtering

---
+++ resources/views/flux/menu/tabs-table.blade.php
@@ -0,0 +1,14 @@
+@props(['statuses', 'filterStatus'])
+
+<div class="flex justify-center mb-6">
+    <div
+        class="flex border rounded-lg bg-gray-100 border-gray-200 dark:border-gray-700 w-full overflow-x-auto whitespace-nowrap md:w-auto scrollbar-hidden">
+        @foreach ($statuses as $status)
+            <flux:button wire:click="$set('filterStatus', '{{ $status }}')" size="sm"
+                variant="{{ $filterStatus === $status ? 'primary' : 'subtle' }}"
+                class="px-2 py-2 m-2">
+                {{ ucfirst($status) }}
+            </flux:button>
+        @endforeach
+    </div>
+</div>
