5377eae006a085c5653f1faa65dca00847270e31
Gilang <19211020@bsi.ac.id>
2025-04-01 20:35:23 +0700

    style(tabs-table): update background color to improve visual consistency

--- resources/views/flux/menu/tabs-table.blade.php
+++ resources/views/flux/menu/tabs-table.blade.php
@@ -2,7 +2,7 @@
 
 <div class="flex justify-center mb-6">
     <div
-        class="flex border rounded-lg bg-gray-100 border-gray-200 dark:border-gray-700 w-full overflow-x-auto whitespace-nowrap md:w-auto scrollbar-hidden">
+        class="flex border rounded-lg bg-zinc-50 border-gray-200 dark:border-gray-700 w-full overflow-x-auto whitespace-nowrap md:w-auto scrollbar-hidden">
         @foreach ($statuses as $status)
             <flux:button wire:click="$set('filterStatus', '{{ $status }}')" size="sm"
                 variant="{{ $filterStatus === $status ? 'primary' : 'subtle' }}"
