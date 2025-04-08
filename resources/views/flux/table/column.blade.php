fd0c354446eb951503ec5b0a42a13a7be2b3f810
Gilang <19211020@bsi.ac.id>
2025-04-08 19:01:08 +0700

    feat(flux/table): add new column component for table headers

---
+++ resources/views/flux/table/column.blade.php
@@ -0,0 +1,4 @@
+<th scope="col"
+    {{ $attributes->merge(['class' => 'px-3 py-3 text-left text-xs font-medium text-gray-900 dark:text-gray-100 uppercase tracking-wider']) }}>
+    {{ $slot }}
+</th>
