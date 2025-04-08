f21d5195cf2067fbec1134a845fce3ee0fe948a2
Gilang <19211020@bsi.ac.id>
2025-04-08 19:00:40 +0700

    refactor(flux/menu): rename tabs-table.blade.php to tabs.blade.php and improve button styling

--- resources/views/flux/menu/tabs.blade.php
+++ resources/views/flux/menu/tabs.blade.php
@@ -2,11 +2,11 @@
 
 <div class="flex justify-center mb-6">
     <div
-        class="flex border rounded-lg bg-zinc-50 border-gray-200 dark:border-gray-700 w-full overflow-x-auto whitespace-nowrap md:w-auto scrollbar-hidden">
+        class="flex rounded-lg bg-zinc-50 dark:bg-zinc-900 border border-gray-200 dark:border-gray-700 w-full overflow-x-auto whitespace-nowrap md:w-auto scrollbar-hidden">
         @foreach ($statuses as $status)
             <flux:button wire:click="$set('filterStatus', '{{ $status }}')" size="sm"
                 variant="{{ $filterStatus === $status ? 'primary' : 'subtle' }}"
-                class="px-2 py-2 m-2">
+                class="px-2 py-2 m-2 {{ $filterStatus === $status ? '' : '' }}">
                 {{ ucfirst($status) }}
             </flux:button>
         @endforeach
