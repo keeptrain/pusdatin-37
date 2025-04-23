6f6dc7f7980bad66f435a9da25af8fe8340bac9a
Gilang <19211020@bsi.ac.id>
2025-04-23 15:31:47 +0700

    feat(request-status): add notes and created_by fields to RequestStatusTrack model and create factory

--- app/Models/Letters/RequestStatusTrack.php
+++ app/Models/Letters/RequestStatusTrack.php
@@ -2,16 +2,20 @@
 
 namespace App\Models\Letters;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RequestStatusTrack extends Model
 {
-
+    use HasFactory;
+    
     protected $table = 'request_status_tracks';
 
     protected $fillable = [
         'letter_id',
         'action',
+        'notes',
+        'created_by'
     ];
 
     public function letter()
