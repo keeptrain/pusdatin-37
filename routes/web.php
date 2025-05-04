789413a1dce9c1a0545efed06e6276a50219c8b6
keeptrain <19211020@bsi.ac.id>
2025-05-04 13:00:21 +0700

    feat(layouts): refactor app layout to use dynamic component based on user role and add dashboard-user view

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use Livewire\Livewire;
+use App\Livewire\Letters\Chat;
 use App\Livewire\Settings\Profile;
 use App\Livewire\Admin\ManageUsers;
-use App\Livewire\Letters\Chat;
 use App\Livewire\Letters\Data\Edit;
 use App\Livewire\Settings\Password;
 use App\Livewire\Letters\DirectForm;
@@ -19,27 +18,43 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('dashboard', function () {
+    $user = auth()->user();
+
+    return match (true) {
+        $user->hasRole(['administrator','verifikator']) => view('dashboard'),
+        $user->hasRole('user') => view('dashboard-user'),
+        default => abort(403),
+    };
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('letter',CreateLetter::class)->name('letter');
     Route::get('letter/upload',UploadForm::class)->name('letter.upload');
     Route::get('letter/form',DirectForm::class)->name('letter.form');
     Route::get('letter/table',ApplicationTable::class)->name('letter.table');
-    // Route::prefix('letter/{id}')->group(function() {
+
+    Route::get('letter/{id}',Detail::class)->name('letter.detail');
+    Route::get('letter/{id}/edit', Edit::class)->name('letter.edit');
+    Route::get('letter/{id}/activity', Activity::class)->name('letter.activity');
+    Route::get('letter/{id}/chat', Chat::class)->name('letter.chat'); 
+    // Route::get('letter/{id}/rollback', Rollback::class)->name('letter.rollback');
+
+        // Route::prefix('letter/{id}')->group(function() {
     //     Route::get('detail', [Detail::class])->name('letter.detail');
     //     Route::get('edit', [Edit::class])->name('letter.edit');
     //     Route::get('activity', [Edit::class])->name('letter.activity');
     //     Route::get('chat', [Edit::class])->name('letter.chat');
     //     // etc
     // });
-    Route::get('letter/{id}',Detail::class)->name('letter.detail');
-    Route::get('letter/{id}/edit', Edit::class)->name('letter.edit');
-    Route::get('letter/{id}/activity', Activity::class)->name('letter.activity');
-    Route::get('letter/{id}/chat', Chat::class)->name('letter.chat');
 });
+
+// Route::group(['middleware' => ['auth','role:administrator']], function () {
+//     Route::prefix('system')->group(function () {
+//         Route::get('users', ManageUsers::class)->name('manage.users');
+//         Route::get('templates', ManageTemplates::class)->name('manage.templates');
+//     });
+// });
 
 Route::prefix('admin')->middleware(['auth', 'role:administrator'])->group(function() {
     Route::get('users', ManageUsers::class)->name('admin.users');
