a1d8b494602b653c1d16e2bb8e8a1b37e86e7464
keeptrain <19211020@bsi.ac.id>
2025-06-06 14:04:38 +0700

    refactor(request-history): combine to 1 table from InformationSystemRequest and PublicRelationRequest

--- resources/views/livewire/letters/history-letter.blade.php
+++ resources/views/livewire/letters/history-letter.blade.php
@@ -1,27 +1,49 @@
-<div x-data="{ activeTab: '{{ $activeTab }}' }">
-  
-  <x-user.header-history />
+<div>
+  <div class="max-w-screen-lg mx-auto">
+    <div class="flex flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0 mb-6">
+      <div>
+        <h2 class="text-2xl font-semibold text-gray-900">History Permohonan</h2>
+        <p class="mt-1 text-sm text-gray-500">
+          Seluruh permohonan layanan yang diajukan terdata disini.
+        </p>
+      </div>
+    </div>
+    <flux:table.base :perPage="$perPage" :paginate="$this->allRequests" emptyMessage="Belum terdapat permohonan">
+      <x-slot name="header">
+        <flux:table.column class="w-1 border-l-2 border-white dark:border-l-zinc-800">
+        </flux:table.column>
+        <flux:table.column>Tanggal diajukan</flux:table.column>
+        <flux:table.column>Jenis Layanan</flux:table.column>
+        <flux:table.column>Status</flux:table.column>
+        <flux:table.column>Aksi</flux:table.column>
+      </x-slot>
 
-  <div class="py-6">
-      <div x-show="activeTab === 'tab1'">
-        @forelse ($this->letters as $letter)
-          <x-user.information-system.history-card :letter="$letter" />
-        @empty
-          <p class="text-center text-gray-500">No requests found.</p>
-        @endforelse
-        <div class="mt-8">
-          {{ $this->letters->links() }}
-        </div>
-      </div>
-      <div x-show="activeTab === 'tab2'">
-        @forelse ($this->publicRelationRequests as $prRequest)
-          <x-user.public-relation.history-card :prRequest="$prRequest"/>
-        @empty
-          <p class="text-center text-gray-500">No requests found.</p>
-        @endempty
-        <div class="mt-8">
-          {{ $this->publicRelationRequests->links() }}
-        </div>
-      </div>
+      <x-slot name="body">
+        @foreach ($this->allRequests as $item)
+      <tr @click="$wire.detailPage({{ $item->id }}, '{{ $item->type }}')" class="hover:bg-zinc-100 cursor-pointer">
+        <flux:table.row>
+
+        </flux:table.row>
+        <flux:table.row>{{ $item->created_at }} </flux:table.row>
+        <flux:table.row>{{ $item->type }}</flux:table.row>
+        <flux:table.row> <flux:notification.status-badge :status="$item->status" /> </flux:table.row>
+        <flux:table.row>
+        <flux:dropdown @click.stop>
+          <flux:button variant="ghost"></flux:button>
+          <flux:menu>
+          </flux:menu>
+        </flux:dropdown>
+        </flux:table.row>
+      </tr>
+    @endforeach
+      </x-slot>
+
+      <x-slot name="emptyRow">
+        <td class="py-3">&nbsp;</td>
+        <td class="py-3">&nbsp;</td>
+        <td class="py-3">&nbsp;</td>
+        <td class="py-3">&nbsp;</td>
+        <td class="py-3">&nbsp;</td>
+      </x-slot>
+    </flux:table.base>
   </div>
-</div>
