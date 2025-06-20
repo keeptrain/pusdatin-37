f91eac5f98361ac79ff9fb78c5d457a9f2426d02
keeptrain <19211020@bsi.ac.id>
2025-06-20 13:23:24 +0700

    refactor: adjust text

--- resources/views/components/user/dashboard/service-cards.blade.php
+++ resources/views/components/user/dashboard/service-cards.blade.php
@@ -1,8 +1,7 @@
 <!-- Service Cards -->
 <div class="grid md:grid-cols-2 gap-6 mt-6">
     <!-- Information & Data Service -->
-    <div
-        class="bg-white rounded-lg p-3 shadow-xs border-slate-200 hover:shadow-lg transition-all duration-300 group">
+    <div class="bg-white rounded-lg p-3 shadow-xs border-slate-200 hover:shadow-lg transition-all duration-300 group">
         <div class="flex items-start justify-between mb-4">
             <div
                 class="w-12 h-12 bg-blue-100 rounded-xl flex items-center justify-center group-hover:bg-blue-200 transition-colors">
@@ -11,7 +10,7 @@
 
         </div>
         <h3 class="text-xl font-medium text-slate-500 mb-2">Layanan Sistem Informasi & Data</h3>
-        <p class="text-slate-600 mb-4">Pengajuan permohonan layanan sistem informasi dan layanan data</p>
+        <p class="text-slate-600 mb-4">Pengajuan permohonan layanan sistem informasi dan data</p>
         <button @click="handleSIDataRequest()"
             class="w-full mt-4 inline-block border bg-zinc-50 text-slate-600 text-sm font-bold px-4 py-2 rounded-md hover:bg-blue-50 transition cursor-pointer">
             Ajukan
@@ -19,14 +18,12 @@
     </div>
 
     <!-- Public Relations Service -->
-    <div
-        class="bg-white rounded-lg p-3 shadow-xs border-slate-200 hover:shadow-lg transition-all duration-300 group">
+    <div class="bg-white rounded-lg p-3 shadow-xs border-slate-200 hover:shadow-lg transition-all duration-300 group">
         <div class="flex items-start justify-between mb-4">
             <div
                 class="w-12 h-12 bg-purple-100 rounded-xl flex items-center justify-center group-hover:bg-purple-200 transition-colors">
                 <x-lucide-file-video class="w-6 text-purple-700" />
             </div>
-
         </div>
         <h3 class="text-xl font-meidum text-slate-500 mb-2">Layanan Kehumasan</h3>
         <p class="text-slate-600 mb-4">Pengajuan permohonan layanan kehumasan</p>
