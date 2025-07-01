59cf73437cc7f778dc13bf8d3ff9f89e95f16934
keeptrain <19211020@bsi.ac.id>
2025-07-01 17:26:16 +0700

    refactor: improve tailwind classes

--- resources/views/components/user/dashboard/warning-modal-sop.blade.php
+++ resources/views/components/user/dashboard/warning-modal-sop.blade.php
@@ -2,7 +2,7 @@
 <div x-show="openModal" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
     x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
     x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-    class="fixed inset-0 z-50 flex items-start justify-center px-4 sm:px-6 mt-20" aria-labelledby="modal-title"
+    class="fixed inset-0 z-50 flex items-start justify-center px-4 sm:px-6 mt-20 overflow-y-auto" aria-labelledby="modal-title"
     role="dialog" aria-modal="true">
 
     {{-- Background overlay --}}
