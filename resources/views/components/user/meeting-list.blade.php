2b860fab9952d671056175716ef9dff665a4ac99
keeptrain <19211020@bsi.ac.id>
2025-07-03 21:13:28 +0700

    fix: bug using tag a when have a link

--- resources/views/components/user/meeting-list.blade.php
+++ resources/views/components/user/meeting-list.blade.php
@@ -35,45 +35,59 @@
         @if($dateGroup['has_meetings'])
             @foreach($dateGroup['meetings'] as $meeting)
                 <div wire:key="meeting-{{ $meeting['id'] }}" class="flex">
-                    <div class="w-[65px]"></div> <!-- Offset untuk border start & end -->
-                    <a href="{{ route('is.show', ['id' => $meeting['request_id']]) }}"
-                        class="border rounded-lg p-3 bg-zinc-50 flex items-center w-125 hover:bg-zinc-100 cursor-pointer" wire:navigate>
+                    <div class="w-[50px]"></div> <!-- Offset untuk border start & end -->
+                    <div class="border rounded-lg p-3 bg-zinc-50 flex items-center w-full">
+                        <!-- Icon -->
                         @if (($meeting['place']['type']) === 'link')
                             <flux:icon.video-camera class="size-12 mr-4 bg-blue-100 text-blue-900 rounded-full p-3" />
+
                         @elseif(($meeting['place']['type']) === 'location')
                             <flux:icon.map-pin class="size-12 mr-4 bg-amber-100 text-amber-900 rounded-full p-3" />
                         @endif
-                        <div class="space-y-0.5">
-                            <flux:text>{{ $meeting['time'] }}</flux:text>
+
+                        <!-- Konten Utama -->
+                        <div class="flex flex-col flex-grow space-y-1.5">
+                            <!-- Lokasi Meeting -->
                             <flux:subheading size="lg" class="text-black text-sm">
                                 @if ($meeting['place']['type'] === 'link')
                                     <flux:link x-data="{
-                                            link: '{{ $meeting['place']['value'] }}',
-                                            isTruncated: false
-                                        }" x-init="
-                                            isTruncated = link.length > 50;
-                                        " :href="$meeting['place']['value']">
+                                                            link: '{{ $meeting['place']['value'] ?? '' }}',
+                                                            isTruncated: false
+                                                        }" x-init="
+                                                            isTruncated = typeof link === 'string' && link.length > 50;
+                                                        " :href="$meeting['place']['value'] ?? '#'">
                                         <span x-text="isTruncated ? link.substring(0, 50) + '...' : link"></span>
                                     </flux:link>
-                                    @if (!empty($meeting['place']['password']))
-                                        <flux:text class="font-mono mt-2">Password: {{ $meeting['place']['password']}}</flux:text>
-                                        {{--
-                                        <flux:input size="sm" variant="filled" :value="$meeting['place']['password']" readonly copyable />
-                                        --}}
+                                    @if ($meeting['place']['password'])
+                                        <flux:text class="font-mono mt-2">Password: {{ $meeting['place']['password'] }}</flux:text>
                                     @endif
                                 @else
                                     {{ $meeting['place']['value'] }}
                                 @endif
                             </flux:subheading>
+
+                            <!-- Topik di Tengah -->
+                            <div class="flex">
+                                <flux:text>Topik: {{ $meeting['topic'] }}</flux:text>
+                            </div>
                         </div>
-                        {{-- <flux:avatar.group class="mt-6">
-                            <flux:avatar circle size="md" initials="AB" />
-                            <flux:avatar circle size="md" initials="CD" />
-                            <flux:avatar circle size="md" initials="EF" />
-                            <flux:avatar circle size="md" initials="GH" />
-                        </flux:avatar.group> --}}
-                    </a>
+
+                        <!-- Jam di Akhir -->
+                        <div class="ml-auto flex items-center space-x-2">
+                            <flux:legend size="lg"><span class="text-gray-400">Jam: </span> {{ $meeting['time'] }}</flux:legend>
+                            @php
+                                $url = !auth()->user()->hasRole('user') ? route('is.show', ['id' => $meeting['request_id']]) : route('detail.request', ['type' => 'information-system', 'id' => $meeting['request_id']]);
+                            @endphp
+                            <flux:button icon="chevron-right" variant="subtle" wire:navigate href="{{ $url }}"></flux:button>
+                        </div>
+                    </div>
                 </div>
+                {{-- <flux:avatar.group class="mt-6">
+                    <flux:avatar circle size="md" initials="AB" />
+                    <flux:avatar circle size="md" initials="CD" />
+                    <flux:avatar circle size="md" initials="EF" />
+                    <flux:avatar circle size="md" initials="GH" />
+                </flux:avatar.group> --}}
             @endforeach
         @endif
     @endforeach
--- resources/views/dashboard-user.blade.php
+++ resources/views/dashboard-user.blade.php
@@ -52,7 +52,7 @@
 
         <flux:separator class="mt-6" />
 
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+        <div class="gap-4 mt-4">
             {{-- {{ $meetingList }} --}}
             <x-user.meeting-list :meetingList="$meetingList" :todayMeetingCount="$todayMeetingCount" />
         </div>
