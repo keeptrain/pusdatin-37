2b73f9ce4139b43b0de4846c28ddbe562878af44
keeptrain <19211020@bsi.ac.id>
2025-07-15 14:27:23 +0700

    refactor: localization language to indonesia

--- resources/views/livewire/auth/forgot-password.blade.php
+++ resources/views/livewire/auth/forgot-password.blade.php
@@ -4,7 +4,7 @@
             <div class="rounded-xl bg-white dark:bg-stone-950 dark:border-stone-800 text-stone-800 shadow-xs">
                 <div class="px-10 py-8">
                     <div class="flex flex-col gap-6">
-                        <x-auth-header :title="__('Forgot password')" :description="__('Enter your email to receive a password reset link')" />
+                        <x-auth-header :title="__('Forgot password')" :description="__('Masukkan email Anda untuk menerima link pengaturan ulang kata sandi')" />
 
                         <!-- Session Status -->
                         <x-auth-session-status class="text-center" :status="session('status')" />
@@ -15,11 +15,12 @@
                                 placeholder="email@example.com" />
 
                             <flux:button variant="primary" type="submit" class="w-full">
-                                {{ __('Email password reset link') }}</flux:button>
+                                {{ __('Email password reset link') }}
+                            </flux:button>
                         </form>
 
                         <div class="space-x-1 text-center text-sm text-zinc-400">
-                            {{ __('Or, return to') }}
+                            {{ __('Atau, kembali ke') }}
                             <flux:link :href="route('login')" wire:navigate>{{ __('log in') }}</flux:link>
                         </div>
                     </div>
--- resources/views/livewire/auth/reset-password.blade.php
+++ resources/views/livewire/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col min-h-screen items-center justify-center bg-gray-100 dark:bg-neutral-900 gap-6 p-4">
+<div class="flex flex-col min-h-svh items-center justify-center gap-6 p-6 md:p-10">
     <!-- Header -->
     <x-auth-header :title="__('Reset password')" :description="__('Silakan masukkan kata sandi baru Anda di bawah ini')" />
 
@@ -6,18 +6,18 @@
     <x-auth-session-status class="text-center" :status="session('status')" />
 
     <!-- Form -->
-    <form wire:submit="resetPassword" class="w-full max-w-md space-y-6">
+    <form wire:submit="resetPassword" class="w-full max-w-sm space-y-6">
         <!-- Password -->
         <flux:input
-                wire:model="password"
-                :label="__('Password')"
-                type="password"
-                required
-                autocomplete="new-password"
-                :placeholder="__('Password')"
-                class="w-full"
-                viewable
-            />
+            wire:model="password"
+            :label="__('Password')"
+            type="password"
+            required
+            autocomplete="new-password"
+            :placeholder="__('Password')"
+            class="w-full"
+            viewable
+        />
 
         <!-- Confirm Password -->
         <flux:input
